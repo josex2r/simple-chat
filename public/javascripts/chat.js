4afedcf8104395800d5a33ddea29a3df081a1c99
Jose Luis Represa <joseluis.represa@bbva.com>
2018-09-19 21:00:23 +0200

    template

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -43,7 +43,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const roomsContent = document.querySelector('#rooms');
   const chatText = document.querySelector('textarea');
   const button = document.querySelector('button#send');
-  let currentRoom;
 
   addAlert(chatContent, 'Waiting server handshake...');
   // addAlert(chatContent, 'Welcome to the <b>Simple chat</b>, REPLACE_USERNAME!');
@@ -51,52 +50,8 @@ document.addEventListener('DOMContentLoaded', () => {
   // addMessage(chatContent, 'REPLACE_USERNAME', new Date(), 'Others message 1', true);
   // addMessage(chatContent, 'REPLACE_USERNAME', new Date(), 'Others message 2', true);
   
-  function joinRoom(newRoom) {
-    socket.emit('change-room', currentRoom, newRoom);
-    currentRoom = newRoom;
-  }
-
-  const socket = io();
-
-  // When the client is connected to the server
-  socket.on('connect', (connect) => {
-    addAlert(chatContent, 'You have been connected!', 'info');
-  });
-
-  // When an information message is received
-  socket.on('info', (message) => {
-    addAlert(chatContent, message, 'info');
-  });
-
-  // When an alert message is received
-  socket.on('alert', (message) => {
-    addAlert(chatContent, message, 'warning');
-  });
-
-  // When client receives the available rooms
-  socket.on('rooms', (rooms) => {
-    currentRoom = rooms[0];
-    clean(roomsContent);
-    rooms.forEach((room) => addRoom(roomsContent, room, joinRoom));
-  });
-
-  // When the client receives a message
-  socket.on('receive-message', ({ message, user, date }) => {
-    addMessage(chatContent, user, new Date(date), message, true);
-  });
-
-  // When the server says that the mesage has been send
-  socket.on('sent-message', ({ message, user, date }) => {
-    addMessage(chatContent, user, new Date(date), message);
-  });
-    
-  // When you disconnects (for example, when the servers gets shutdown)
-  socket.on('disconnect', () => {
-    addAlert(chatContent, 'You have been disconnected :(', 'danger');
-  });
-
   // Send a message to the server when the button is clicked
   button.addEventListener('click', () => {
-    socket.emit('send-message', chatText.value, new Date(), currentRoom);
+    // Click callback
   });
 });
