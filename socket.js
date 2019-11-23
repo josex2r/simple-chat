bcc94515f6dfcdcc80e2c1a36e8b7fd073235bbb
Alejandro Sánchez Pineda <alsapi2004@gmail.com>
2019-11-23 15:11:07 +0100

    use cookie in socket

--- socket.js
+++ socket.js
@@ -2,13 +2,23 @@
  * Module for handling socket connections
  */
 const socketIo = require('socket.io');
+const cookie = require('./middlewares/cookie');
+
 
 module.exports = server => {
   const io = socketIo(server);
 
+  io.use((socket, next) => {
+    // socket.request
+    // socket.request.res
+    cookie(socket.request, socket.request.res, next);
+  });
+
   io.on('connection', socket => {
     socket.emit('chat-ready', socket.id);
-    socket.broadcast.emit('user-join');
+    // el nombre de usuario se mete en la
+    // propiedad user de la cookie session
+    socket.broadcast.emit('user-join', socket.request.session.user);
   });
 };
 
